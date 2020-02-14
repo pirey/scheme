10760296fd0a0e21df338c8fa78ff64417c2ea99
Yeri <arifyeripratama@gmail.com>
2020-02-14 21:37:02 +0700

    IO

---
+++ examples/hello.scm
@@ -0,0 +1,4 @@
+(define (tambah x y) (+ x y))
+(define (kurang x y) (- x y))
+
+(tambah 1 (kurang 2 35))
--- src/Scheme/Current.hs
+++ src/Scheme/Current.hs
@@ -1,6 +1,6 @@
 module Scheme.Current where
 
-import Scheme.Function as Scheme
+import Scheme.IO as Scheme
 
 main_ :: IO ()
 main_ = Scheme.main_
